9a3fa97dc5a7aca1be7db9ddd907bdc87f62a230
adel-elmala <adel.elmala2025@gmail.com>
2023-02-15 19:19:12 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Commands Trace
+
+- git init
+- touch README.md
+- git add README.md
+- git commit -m "init commit"
+- git branch -M main
+- git remote add origin git@github.com:adel-elmala/versionControlAssignment_siemens.git
+- git push -u origin main
+- git add main.cpp Makefile
+- git commit -m "getSum"
+- git push -u origin main
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getSum(const vector<int> &v);
+
+int main(int argc, char const *argv[])
+{
+    vector<int> testVec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    cout << getSum(testVec) << endl;
+    return 0;
+}
+
+int getSum(const vector<int> &v)
+{
+    int sum = 0;
+    for (const int &n : v)
+        sum += n;
+    return sum;
+}
