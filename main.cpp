54271004dc939e17626926c1d6b9a5a9939a72a1
adel-elmala <adel.elmala2025@gmail.com>
2023-02-15 19:21:14 +0200

    getAvg implemented

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 int getSum(const vector<int> &v);
+int getAverage(const vector<int> &v);
 
 int main(int argc, char const *argv[])
 {
     vector<int> testVec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     cout << getSum(testVec) << endl;
+    cout << getAverage(testVec) << endl;
     return 0;
 }
 
@@ -18,4 +20,11 @@ int getSum(const vector<int> &v)
     for (const int &n : v)
         sum += n;
     return sum;
+}
+
+int getAverage(const vector<int> &v)
+{
+    int sum = getSum(v);
+
+    return (sum / v.size());
 }
